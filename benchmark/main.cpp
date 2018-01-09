360e20eb9b276a8909da0359f4d53a6bac5dd0f7
Geoffrey Hunter <ghunter@urthecast.com>
2018-01-08 17:57:51 -0800

    Adding code to perform manual floating point addition.

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -13,6 +13,8 @@
 #include <sys/resource.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdexcept>
+#include <iostream>
 
 // User includes
 #include "MFixedPoint/FpF.hpp"
@@ -105,7 +107,121 @@ void PrintMetrics(time_measure * tu, char* testName, uint32_t testCount, double 
 		percGreaterThanAvg);
 }
 
+#define min_float 0x00000000
+#define max_float 0xffffffff
+
+    #define exponent(x) (x << 1) >> 24
+    #define mantissa(x) (x << 9) >> 9
+    #define sign(x) x >> 31
+
+uint32_t add(uint32_t x, uint32_t y) {
+    uint32_t result_mantissa;
+    uint32_t result_exponent;
+    uint32_t result_sign;
+
+    uint32_t different_sign = sign(x) ^ sign(y); //boolean but lets not do any type casting
+
+    // catch NaN
+    if (!(exponent(x) ^ 0xFF) && mantissa(x)) return x;
+    if (!(exponent(y) ^ 0xFF) && mantissa(y)) return y;
+
+    // catch Inf
+    if (!(exponent(x) ^ 0xFF) && !(exponent(y) ^ 0xFF)) {
+        // both are inf
+        if (different_sign)
+            // Inf - Inf
+            return 0x7F800000 + 1; // NaN
+        else
+            // both Inf or -Inf
+            return x;
+    }
+    else if (!(exponent(x) ^ 0xFF)) return x;
+    else if (!(exponent(y) ^ 0xFF)) return y;
+
+    // both numbers are non-special
+    uint32_t exp_difference;
+    if (different_sign) {
+        exp_difference = exponent(y) + exponent(x);
+    }
+    else {
+        // no need to account for constant BO
+        // beware of underflow
+        if (exponent(x) > exponent(y)) exp_difference = exponent(x) - exponent(y);
+        else exp_difference = exponent(y) - exponent(x);
+    }
+
+
+    bool x_bigger_abs;
+    if      (exponent(x) > exponent(y)) x_bigger_abs = true;
+    else if (exponent(x) < exponent(y)) x_bigger_abs = false;
+    else if (mantissa(x) > mantissa(y)) x_bigger_abs = true;
+    else                                x_bigger_abs = false;
+
+    if (!different_sign) {
+        //both numbers have same sign (this is a sum)
+        result_sign = sign(x);
+
+        if (x_bigger_abs) {
+            result_mantissa = (mantissa(x) << 1) + (mantissa(y) << 1) >> exp_difference;
+            result_exponent = exponent(x);
+        }
+        else {
+            result_mantissa = (mantissa(y) << 1) + ((mantissa(x) << 1) >> exp_difference);
+            result_exponent = exponent(y);
+        }
+        if (result_mantissa << 31) result_mantissa = (result_mantissa >> 1) + 1;
+        else result_mantissa = (result_mantissa >> 1);
+    }
+    else {
+        // this actually is a subtraction
+
+        if (x_bigger_abs) {
+            result_sign = sign(x);
+            result_exponent = exponent(x);
+
+            // subtract and round to 23 bit 
+            // this means making room in our 32bit representation
+            result_mantissa = (mantissa(x) << 1) - ((mantissa(y) << 1) >> exp_difference );
+        }
+        else {
+            result_sign = sign(y);
+            result_exponent = exponent(y);
+
+            // subtract and round to 23 bit 
+            // this means making room in our 32bit representation
+            result_mantissa = (mantissa(y) << 1) - ((mantissa(x) << 1) >> exp_difference);
+        }
+
+        if (result_mantissa << 31)  result_mantissa = ((result_mantissa >> 1) + 1);
+        else result_mantissa = (result_mantissa >> 1);
+
+        // normalize mantissa
+        uint32_t temp = result_mantissa << 9;
+        for (uint32_t count = 0; count < 23; ++count) {
+            if (!((temp << count) >> 31)) {
+                result_mantissa <<= ++count; // leading 1, so shift 1 more time
+                result_exponent -= count;
+                break;
+            }
+        }
+    }
+    uint32_t result = result_sign << 31 | result_exponent << 23 | result_mantissa;
+    return result;
+}
+
 int main() {
+
+	// // Make sure our custom float addition works
+	// float testFloat1 = 1.0f;
+	// float testFloat2 = 2.0f;
+	// uint32_t rawResult = add(reinterpret_cast<uint32_t&>(testFloat1), reinterpret_cast<uint32_t&>(testFloat2));
+	// float result = reinterpret_cast<float&>(rawResult);
+	// if(result != 4.32 + 7.89) {
+	// 	std::cout << "result = " << result;
+	// 	throw std::runtime_error("add() did not work.");
+	// }
+
+
 	FpF<int32_t, 8> fp1(5.6);
 	FpF<int32_t, 8> fp2(8.9);
 	FpF<int32_t, 8> fp3;
@@ -207,7 +323,8 @@ int main() {
 	{	
 		int x = 0;
 		for(x = 0; x < NUM_TESTS; x++) {
-			float3 = float1 - float2;
+			// uint32_t rawFloat = add(*(uint32_t*)&float1, *(uint32_t*)&float2);
+			float3 = float1 + float2;
 		}
 	}	
 	StopTimeMeasuring(tu);
